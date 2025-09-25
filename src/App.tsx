d28b311237a8c5fb26b44edbbf82cbc240fa9ad0
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-25 20:55:05 -0300

    feat: layout comcluido

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-
+import { Routes, Route } from "react-router-dom";
+import { Layout } from "./components/layout";
+import { AddStudy } from "./pages/add-study";
+import { useState } from "react";
+import type { StudySession } from "./types/study";
 
 function App() {
+  const [studies, setStudies] = useState<StudySession[]>([]);
+
+  const handleAddStudy = (study: StudySession) => {
+    setStudies((prevStudies) => [...prevStudies, study]);
+  };
 
   return (
-    <>
-      
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route
+          index
+          element={<h1>Home</h1>}
+        />
+        <Route
+          path="add"
+          element={
+            <AddStudy
+              onAdd={handleAddStudy}
+              studies={studies}
+            />
+          }
+        />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
